e42e5ea738e553433d8cc9081fa193772a4d1bde
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-07 00:36:11 -0400

    Time: 42 ms (68.56%), Space: 14 MB (48.95%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -3,7 +3,6 @@ class Solution:
         ans=""
         lens = [len(i) for i in strs]
         minLen = min(lens)
-        print(minLen)
         
         if len(strs) == 1:
             ans = strs[0]
@@ -11,22 +10,16 @@ class Solution:
         for i in range(minLen):
             flag=0
             for j in range(1,len(strs)):
-                print(strs[j-1][i], strs[j][i])
                 if strs[j-1][i] == strs[j][i]:
                     if flag == 0:
                         ans = ans + strs[j][i]
                         flag=1    
                 else:
-                    print("else=0--==",ans,flag)
                     if flag == 1:
                         ans = ans[:-1]
-                        break 
-                    print("ans else=",ans)     
+                        break    
                     return ans
-                    
-                    
-        print("ans=",ans)  
-        
+
         return ans
             
         
