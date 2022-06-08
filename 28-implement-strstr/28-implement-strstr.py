7685e481412be82a97b853edf9f009b90687bc7f
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-08 00:35:38 -0400

    Time: 52 ms (25.61%), Space: 13.9 MB (15.29%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -2,8 +2,6 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
         if needle in haystack:
-            ind = haystack.index(needle)
-            print("ind-",ind)
-            return ind
+            return haystack.index(needle)
         else:
             return -1
