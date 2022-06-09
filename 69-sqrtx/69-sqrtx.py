60ed46ab3d041ab5d4ddc5b99f0d3abac4752bcd
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-08 23:55:16 -0400

    Time: 1660 ms (18.34%), Space: 14 MB (10.64%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -3,32 +3,22 @@ class Solution:
         
         copy = x
         
-        if x == 0:
-            return 0
-        
-        if x == 1:
-            return 1
+        if x == 0 or x == 1:
+            return x
         
         while copy > 0:
             prev = copy
-            copy = copy/2
-            copy = int(copy)
-            
-            # print("cpp",copy)
+            copy = int(copy/2)
             
             if copy*copy == x:
                 return int(copy)
             
             if copy*copy <x:
-                # print("in 1")
                 for i in range(int(copy), int(prev)):
                     if i * i == x:
-                        # print("in 2")
                         return i
                     else:
-                        # print("in 3",i,copy)
                         if i*i <x and (i+1)*(i+1) >x:
-                            # print("in 4")
                             return i
             
         
