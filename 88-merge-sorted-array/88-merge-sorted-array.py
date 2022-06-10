5c7893febcb0d2c98f28227930c8dd0ce21d03c1
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-10 16:35:07 -0400

    Time: 76 ms (10.13%), Space: 13.9 MB (85.09%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,18 +4,13 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        # if m == 1:
-        #     print(nums1)
         c=0
         nums1.extend(nums2)
         nums1.sort()
-        print("--==",nums1)
         for i in range(m+n):
-            print("--",nums1)
             if 0 in nums1 and n!=0:
                 nums1.remove(0)
                 c+=1
             if c == n:
                 break
-        print(nums1)
         
