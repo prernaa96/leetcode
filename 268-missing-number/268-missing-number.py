f8b15a9801ac9865f2ec25a2cdd51903e3d7a204
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-22 00:12:26 -0400

    Time: 203 ms (50.79%), Space: 15.2 MB (33.10%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        sum1=0
+        sum2=0
+        for i in range(len(nums)):
+            sum1+=i+1
+            sum2+=nums[i]
+            
+        # print(sum1-sum2)    
+        return sum1-sum2
