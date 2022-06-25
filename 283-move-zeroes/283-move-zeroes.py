869a2b75d6a0624c2f550389cd58487dbd208037
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-06-25 13:06:30 -0400

    Time: 1182 ms (10.52%), Space: 15.5 MB (96.79%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,18 +4,9 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        
-        # if len(nums) == 1:
-        #     return nums
-        
-        c = nums.count(0)
-        # if c == len(nums):
-        #     return nums
-        
-        for i in range(c):
+        for i in range(nums.count(0)):
             if 0 in nums:
-                nums.append(nums.pop(nums.index(0)))  
-        # print(nums)    
+                nums.append(nums.pop(nums.index(0)))    
         
         
       
