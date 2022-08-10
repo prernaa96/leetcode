fd3a151addc38ba960bb1adf07eca210b740be06
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-08-09 20:33:03 -0400

    Time: 39 ms (87.16%), Space: 13.9 MB (14.36%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        if n ==1:
+            return True
+       
+        checkset = set()
+        def calc(n):
+            total=0
+            while n!=0:
+                a = n% 10
+                total = a*a + total
+                n = n// 10
+            return total    
+       
+        total = calc(n)
+     
+        if total == 1:
+            return True
+        
+        while total != 1:
+            total=calc(total)
+           
+            if total in checkset:
+                return False
+            else:
+                if total ==1:
+                    return True
+                else:
+                    checkset.add(total)      
+          
+    
+    
