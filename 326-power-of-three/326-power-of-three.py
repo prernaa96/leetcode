3b6a154d29e8f38221888042a219dd897c4385e4
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-08-10 17:23:35 -0400

    Time: 154 ms (34.81%), Space: 13.9 MB (57.78%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -9,14 +9,11 @@ class Solution:
         
         for i in range(n):
             n = n/3
-            print(n)
             
             if n % 3 != 0:
                 if n != 1:
-                    # print("in")
                     return False
             
             if n == 1:
-                # print("true")
                 return True
             
