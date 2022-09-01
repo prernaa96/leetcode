512fa4ecf69d5825e3a23c5537574c7f14c61266
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-01 15:47:19 -0400

    Time: 38 ms (85.54%), Space: 13.7 MB (97.14%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        
+        strx = str(x)
+        strx_rev = strx[::-1]
+        if x < 0:
+            strx_rev = strx_rev[:len(strx_rev)-1]
+            strx_rev = '-' + strx_rev
+        # print((int(strx_rev)))
+        
+        if x <= (-2 ** 31) or x >= ((2 ** 31)-1):
+            return 0
+        elif int(strx_rev) <= (-2 ** 31) or int(strx_rev) >= ((2 ** 31)-1):
+            return 0
+        return int(strx_rev)
