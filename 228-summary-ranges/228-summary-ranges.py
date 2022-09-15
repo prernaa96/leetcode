760ca2d46fc5385df0fabd634ce404d28866afba
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-15 09:39:00 -0400

    Time: 57 ms (23.53%), Space: 13.8 MB (98.24%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -1,13 +1,9 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         
-        string=""
-        start=-1
-        end = 2**31-1
         count=0
         maxcount=-1
         ans=[]
-        i=1
         
         for i in range(1,len(nums)):
             if nums[i-1]+1 == nums[i]:
@@ -15,14 +11,11 @@ class Solution:
                 continue
             elif count>0:
                 ans.append(f"{nums[i-count-1]}->{nums[i-1]}")
-                # print("--",ans)
                 count=0    
             else:
                 ans.append(str(nums[i-1]))
-            print(ans,count,i)   
             
         if count>0:
-            # print("here")
             ans.append(f"{nums[i-count]}->{nums[i]}")
         if not count and len(nums) >1:
             ans.append(str(nums[i]))
