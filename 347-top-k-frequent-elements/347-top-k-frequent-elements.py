f975c94fa9cf2d7c8775502187a00d2c987637fc
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-16 00:14:38 -0400

    Time: 414 ms (5.04%), Space: 18.8 MB (48.27%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,7 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        c=1
         dic={}
         
         if len(nums)==k:
@@ -9,17 +8,14 @@ class Solution:
         
         for i in range(len(nums)):
             dic[nums[i]] = dic.get(nums[i],0)+1
-        # print(dic)
         
         maxval=-10**4-1
         maxarr=[]
         
         for m,v in list(dic.items()):
             maxval=max(dic,key=dic.get)
-            # print(maxval, dic[maxval])
             maxarr.append(maxval)
             del dic[maxval]
-            # print("--",dic,maxarr)
             if len(maxarr) == k:
                 return maxarr
             
