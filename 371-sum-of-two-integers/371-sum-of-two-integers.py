15f7673029051dfff411be293f210ec91c2a550b
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-18 14:08:50 -0400

    Time: 61 ms (14.58%), Space: 13.9 MB (62.35%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,15 +1,17 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         
-        if a < 0 and b > 0 or a>0 and b<0:
-            while b!=0 :
-                andshift = (a&b) & 0xFFFFFFFF
-                a= (a^b) & 0xFFFFFFFF
-                b = (andshift <<1) 
+        if not a :
+            return b
+        if not b:
             return a
-        else:
-            while b!=0 :
-                andshift = (a&b) 
-                a= (a^b) 
-                b = (andshift <<1) 
-            return a
+         
+        while b!=0 :
+            andshift = (a&b) & 0xffffffff
+            a= (a^b) & 0xffffffff
+            b = (andshift <<1) 
+            
+        if (a>>31)&1:
+            return ~(a^0xffffffff)
+            
+        return a
