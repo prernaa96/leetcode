b949045237edb0c0e7d4c646be86ff0d73c8779f
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-18 14:22:17 -0400

    Time: 62 ms (16.46%), Space: 14 MB (14.16%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,10 +1,4 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         
-        xor = x^y
-        ans=[]
-        
-        for i in bin(xor):
-            ans.append(i)  
-        tot = ans.count('1')
-        return tot
+        return bin(x^y).count('1')
