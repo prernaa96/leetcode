fa2b75c1fa4c366f74e9a327cc658701304e8743
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-21 00:47:10 -0400

    Time: 114 ms (18.89%), Space: 15.5 MB (51.26%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -4,10 +4,7 @@ class Solution:
         setnum = set(nums)
         if len(setnum)<3:
             return max(setnum)
-        maxval = max(setnum)
-        setnum.remove(maxval)
-        maxval = max(setnum)
-        setnum.remove(maxval)
-        return max(setnum)
+        sort = sorted(setnum)
+        return sort[len(sort)-3]
         # print(setnum)
         
