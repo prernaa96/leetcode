d1dd8a70f29eb67fb22525df328569a038ac1867
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-23 17:34:23 -0400

    Time: 77 ms (17.48%), Space: 13.8 MB (76.21%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        dic1={}
+        dic2={}
+        
+        for i in (t):
+            dic2[i] = dic2.get(i,0)+1
+        for j in s:
+            dic1[j] = dic1.get(j,0)+1    
+        print(dic1,dic2)
+        
+        res = {key: dic2[key] - dic1.get(key, 0)
+                       for key in dic2.keys()}
+        for i in res:
+            if res[i]!=0:
+                return i
