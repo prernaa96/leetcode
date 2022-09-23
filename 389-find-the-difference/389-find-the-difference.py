7d45739a26f8f758da132f0e8563239f987de80d
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-23 17:35:51 -0400

    Time: 76 ms (18.84%), Space: 13.9 MB (76.21%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -8,7 +8,6 @@ class Solution:
             dic2[i] = dic2.get(i,0)+1
         for j in s:
             dic1[j] = dic1.get(j,0)+1    
-        print(dic1,dic2)
         
         res = {key: dic2[key] - dic1.get(key, 0)
                        for key in dic2.keys()}
