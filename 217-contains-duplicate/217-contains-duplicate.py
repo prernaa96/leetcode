d7ed5dcade9cb407f72f9ca55828cf8504d19fca
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-25 10:32:06 -0400

    Time: 1510 ms (5.01%), Space: 25.7 MB (96.01%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -7,11 +7,8 @@ class Solution:
         dic={}
         for i in range (len(nums)):
             dic[nums[i]] = dic.get(nums[i],0)+1
-            
-        # print(dic)
         
         for k,v in dic.items():
-            # print(dic[k])
             if dic[k] > 1:
                 return True
         return False    
