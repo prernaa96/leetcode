bf18105afa2fa53e1199b08f88e80bed1ae6d9ab
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-26 10:22:48 -0400

    Time: 6846 ms (5.01%), Space: 14 MB (13.03%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -3,17 +3,14 @@ class Solution:
         
         c=0
         string=""
-        
         res = 1
      
         for i in range(2, n+1):
             res *= i
-        
         a=str(res)
         if '0' not in a:
             return 0
         b=a[::-1]
-        # print(b)
         for i in range(len(b)):
             if b[i]!='0':
                 string = b[:i]
