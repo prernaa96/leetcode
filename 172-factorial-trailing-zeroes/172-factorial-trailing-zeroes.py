0a27d5485045f50b44349601f3e83341fb69f7a8
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-09-27 11:05:49 -0400

    Time: 68 ms (42.08%), Space: 14 MB (13.14%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -2,17 +2,33 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         
         c=0
-        string=""
-        res = 1
+
+        a = n/5
+        b = a//5
+        n=a+b
+        print(n)
+
+        while b>=5:
+            b=b//5
+            c=c+b
+            print(b,c)
+        n=n+c
+            
+        return int(n)
+        
+        
+#         c=0
+#         string=""
+#         res = 1
      
-        for i in range(2, n+1):
-            res *= i
-        a=str(res)
-        if '0' not in a:
-            return 0
-        b=a[::-1]
-        for i in range(len(b)):
-            if b[i]!='0':
-                string = b[:i]
-                break
-        return len(string)
+#         for i in range(2, n+1):
+#             res *= i
+#         a=str(res)
+#         if '0' not in a:
+#             return 0
+#         b=a[::-1]
+#         for i in range(len(b)):
+#             if b[i]!='0':
+#                 string = b[:i]
+#                 break
+#         return len(string)
