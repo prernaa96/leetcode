250978f23cec0e5c2dcf1d9f0ab41ee934338682
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-01 18:47:01 -0400

    Time: 81 ms (5.63%), Space: 13.8 MB (66.67%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        
+        dic={}
+        flag=False
+        for i in range(len(s)):
+            dic[s[i]] = dic.get(s[i],0)+1
+        print(dic)
+        
+        c=0
+        maxval=0
+        for v in dic.values():
+            if v%2 == 0:
+                c=c+v
+            else:
+                c=c+v-1
+                flag=True
+        
+        if flag==True:
+            c=c+1
+        print(c)
+        
+        return c
+                
