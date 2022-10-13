88ccbc7417fe25fe689126436631dbf3e5be7405
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-13 10:00:18 -0400

    Time: 108 ms (52.20%), Space: 13.8 MB (61.71%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -4,7 +4,6 @@ class Solution:
         total_sum=0
         initial_sum=sum(arr)
         total_sum+=initial_sum
-        print("1-",total_sum)
        
         if len(arr)<3:
             return total_sum
@@ -12,27 +11,20 @@ class Solution:
         length=3
         
         while(length<len(arr)):
-            print("length----",length)
             
             curr=0
             
             for i in range(length):
                 curr=curr+arr[i]
-            print("initial curr-",curr)
             total_sum+=curr
-            print("tot=",total_sum)
             
             for i in range(length,len(arr)):
                 curr=curr+arr[i]-arr[i-length]
-                print("------>",curr,arr[i], arr[i-length])
                 total_sum+=curr
-                print("total_sum=",total_sum)
             length+=2
             
         if len(arr)%2!=0:
             total_sum+=sum(arr)
-                
-        
         return total_sum
             
            
