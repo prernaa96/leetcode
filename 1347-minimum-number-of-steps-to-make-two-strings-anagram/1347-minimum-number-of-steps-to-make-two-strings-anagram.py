51319908dc6c6bd0eeecd85960d5947c554aca8d
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-19 10:06:16 -0400

    Time: 198 ms (85.84%), Space: 14.5 MB (77.66%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -6,20 +6,14 @@ class Solution:
         s_count=Counter(s)
         t_count=Counter(t)
         
-        print(s_count,t_count)
         if(s_count==t_count):
             return 0
         
         for i in t_count:
-            # print(t_count[i],s_count[i])
             if t_count[i]>s_count[i]:
-                print("i if-",i)
                 ans+=t_count[i]-s_count[i]
-                print("ans if-",ans)
             elif i not in s_count:
-                print("i elif-",i)
                 ans+=1
-                print("ans elif-",ans)
                 
         return ans
         
