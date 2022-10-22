675fe9bac4348293059707c2a5d190054f19a651
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-22 11:00:34 -0400

    Time: 194 ms (9.24%), Space: 14 MB (33.64%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.py
@@ -7,22 +7,14 @@ class Solution:
         
         diff=brackets[0][0]
         for i in range(len(brackets)):
-            # print("diff=",diff)
             check+=diff
-            # print("check==",check)
             if check>income:
                 prev=check-diff
-                print("******",prev,check,diff)
                 diff=abs(income-prev)
                 flag=True
             ans+=(diff* (brackets[i][1])/100)
-            print("-----",(diff* (brackets[i][1])/100), ans )
             if i+1<len(brackets):
                 diff = brackets[i+1][0]-brackets[i][0]
             if flag==True:
-                break
-            
-            print(ans,diff,check)
-            
-        
+                break        
         return ans
