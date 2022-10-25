d4b5d7204fe8c210f683c19167fe1daeba9b5694
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-25 19:41:50 -0400

    Time: 1487 ms (32.53%), Space: 32.2 MB (62.75%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -8,13 +8,9 @@ class Solution:
         
         for i in arr:
             dic[i]=dic.get(i,0)+1
-        print()
         
         for k in sorted(dic, key=dic.get, reverse=True):
-            print("k=",k)
             c+=dic[k]
             c1+=1
-            print(c)
             if c>=n:
-                print("cond--",c)
                 return c1
