36287a6218b6dfdcbd1fd752d7193d46f3ebff16
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-10-31 10:46:48 -0400

    Time: 45 ms (81.07%), Space: 14 MB (78.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -5,17 +5,21 @@ class Solution:
         # 2. Put words one by one in string
         # 3. Use strip to remove white spaces
         
-        string=""
-        string1=""
-        s=s.strip()
-        # print("s=",s)
+        ns=s.split()
+        ns.reverse()
+        return ' '.join(ns)
+
         
-        for i in range(len(s)-1,-1,-1):
-            string1=s[i] + string1
-            # print("string1-",string1)
-            if s[i] == " " or i==0:
-                string=(string+" "+string1.strip()).strip()
-                string1=" "
-            # print("string-",string)
+        
+        
+#         string=""
+#         string1=""
+#         s=s.strip()
+        
+#         for i in range(len(s)-1,-1,-1):
+#             string1=s[i] + string1
+#             if s[i] == " " or i==0:
+#                 string=(string+" "+string1.strip()).strip()
+#                 string1=" "
             
-        return string.strip()
+#         return string.strip()
