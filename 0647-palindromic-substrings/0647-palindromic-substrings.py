bc95773bc97bb8f8f1ebe890cb28fa6d1270aeb4
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-11-05 11:15:26 -0400

    Time: 1697 ms (5.00%), Space: 13.9 MB (38.22%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -4,24 +4,14 @@ class Solution:
         
         # 1. If string!=palindrome then output=len(s)
         # 2. ababa, gfhgfgjhjk
-        
-        # if s != s[::-1]:
-        #     return len(s)
+
         
         c=0
-        count=0
-        
-        # if s==s[::-1]:
+  
         for i in range(len(s)):
             for j in range(len(s),-1,-1):
-                # print(i,j, s[i:j], s[i:j][::-1])
                 if i==j:
                     break
                 if s[i:j] == s[i:j][::-1]:
-                    # print( s[i:j], s[i:j][::-1])
                     c+=1
-                
-             
-                # print("c==",c)
-
         return c
