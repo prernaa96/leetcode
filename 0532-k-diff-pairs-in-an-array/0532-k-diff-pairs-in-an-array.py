07b1ef65abd3f9d3c5cf3a040a7744fc39cd5489
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-11-07 11:15:45 -0500

    Time: 4455 ms (5.05%), Space: 16.9 MB (7.68%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -11,19 +11,15 @@ class Solution:
             else:
                 val = nums[i]-k
             dic[nums[i]] = val
+
             
-        print(dic)
-        
         for k,v in dic.items():
             if v in nums:
                 if (v,k) not in ans:
                     
                     if k==v and nums.count(v)<2:
-                        print("----")
                         continue
+                        
                     ans.add((k,v))
-                
-        print(ans)
-        
         return len(ans)
         
