d2b8463a92716893b528525fe493db1558ff6067
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-11-12 14:59:05 -0500

    Time: 2627 ms (26.23%), Space: 33.2 MB (69.80%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         
+        #Use dict + prefix sum + mod!
+          
         prefixSum = {0:-1}
         sums=0
         for i in range(len(nums)):
             sums+=nums[i]
             res=sums%k
             if res in prefixSum:
+                #if length is greater than 2 (as per question)
                 if i-prefixSum[res]>1:
                     return True
             else:
                 prefixSum[res]=i
         
         return False
-        
+            
+     
         
         
         
