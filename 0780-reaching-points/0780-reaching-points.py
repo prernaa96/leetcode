28f7504b61e1efe7d0e9209a03b64edb4b75cb11
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-11-13 17:57:12 -0500

    Time: 38 ms (84.00%), Space: 13.9 MB (34.54%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.py
+++ 0780-reaching-points/0780-reaching-points.py
@@ -1,12 +1,16 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
         
+        #Mod of tx = tx%ty and ty=ty%tx
+        
         if sx==sy==tx==ty: return True
         
         while sx<tx and sy<ty:
             tx,ty=tx%ty,ty%tx
-            print(tx,ty)
 
+        #Main = (ty-sy)%sx ==0 and (tx-sx)%sy==0
+        #Rest - edge cases
+        
         if tx!=0 and ty!=0 :
             if sx==tx and sy<=ty and (ty-sy)%sx ==0 or sy==ty and sx<=tx and (tx-sx)%sy==0:
                 return True
