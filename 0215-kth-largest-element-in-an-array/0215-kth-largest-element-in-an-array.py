4d002608d3c434f565b262fcb42b20e85368bc9c
Prerna Achtani <73582366+prernaa96@users.noreply.github.com>
2022-11-29 19:31:27 -0500

    Time: 1484 ms (38.12%), Space: 27.2 MB (64.91%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,14 +4,17 @@ class Solution:
         heap=[]
         heapify(heap)
         
+        #Add all elements to heap
         for i in nums:
             heappush(heap,i)
-        print(heap)
+        # print(heap)
         
+        #Find number of elements to pop
         ranges = len(nums)-k
         
+        #Get first element of remaining elements which will be minimum of elements in list byt kth larget num
         for i in range(ranges):
             heappop(heap)
-        print(heap)
+        # print(heap)
         
         return heap[0]
